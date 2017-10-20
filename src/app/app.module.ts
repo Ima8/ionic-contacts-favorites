e90b23817d1c49d41f4250d423764afeab901a69
Issaret Prachimutita <imac.monochrome@gmail.com>
2017-10-20 14:37:25 +0700

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { RecentsPage } from '../pages/recents/recents';
+import { ContactsPage } from '../pages/contacts/contacts';
+import { ContactDetailPage } from '../pages/contact-detail/contact-detail';
+import { ContactEditPage } from '../pages/contact-edit/contact-edit' ;
+import { FavoritesPage } from '../pages/favorites/favorites' ;
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { ContactDataProvider } from '../providers/contact-data/contact-data';
+
+import { HttpModule } from '@angular/http' ;
+import { FormsModule } from '@angular/forms' ;
+
+@NgModule({
+  declarations: [
+    MyApp,
+    RecentsPage,
+    ContactsPage,
+    ContactDetailPage,
+    ContactEditPage,
+    FavoritesPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    RecentsPage,
+    ContactsPage,
+    ContactDetailPage,
+    ContactEditPage,
+    FavoritesPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactDataProvider
+  ]
+})
+export class AppModule {}
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { RecentsPage } from '../recents/recents';
+import { ContactsPage } from '../contacts/contacts';
+import { FavoritesPage } from '../favorites/favorites' ;
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = ContactsPage;
+  tab2Root = RecentsPage;
+  tab3Root = FavoritesPage ;
+
+  constructor() {
+
+  }
+}
